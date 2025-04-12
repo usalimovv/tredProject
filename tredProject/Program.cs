bd7e34917d2ea44375d7f8f7e7647cb80779267e
usalimovv <umidsalimov2004@gmail.com>
2025-04-12 23:39:28 +0500

    qaiiii

--- tredProject/Program.cs
+++ tredProject/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("git hubdan reporstori yaratish ");
+            Console.WriteLine("git hubdan reporstori yaratish " ); 
         }
     }
 }

d154d99de530b557d1ac25273a11ab2559eb5dce
usalimovv <umidsalimov2004@gmail.com>
2025-04-12 23:39:03 +0500

    ko'rilik

---
+++ tredProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tredProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("git hubdan reporstori yaratish ");
+        }
+    }
+}
